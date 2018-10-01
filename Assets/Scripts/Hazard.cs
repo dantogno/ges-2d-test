0c39815adfedd3b91809c43b4787a1b57da8b5b9
dantogno <vebulidbih@msn.com>
2018-10-01 11:18:34 -0500

    new tileset demo level

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    [SerializeField]
+    private float test = 1;
+    private void Update()
+    {
+        Debug.Log("test");
+    }
+    private void Start()
+    {
+        Debug.Log("Test");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player hit hazard!");
+        }
+    }
+
+}
